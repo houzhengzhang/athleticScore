49ee9f07be1ce5d8c1c61fd3a4f9a90eeadbf885
houzhengzhang <976914833@qq.com>
2019-05-10 14:03:08 +0800

    update sql

--- athletic.sql
+++ athletic.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50640
 File Encoding         : 65001
 
-Date: 2019-05-10 13:54:10
+Date: 2019-05-10 14:02:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,7 +34,7 @@ CREATE TABLE `adminstrator` (
 -- ----------------------------
 -- Records of adminstrator
 -- ----------------------------
-INSERT INTO `adminstrator` VALUES (''C33251F1E4CE4FF191433D619D8984C5'', ''123@qq.com'', ''123456'', ''admin'', ''1'', ''F10079D3840F4411A324B8DF7483FE3F'');
+INSERT INTO `adminstrator` VALUES ('C33251F1E4CE4FF191433D619D8984C5', '123@qq.com', '123456', 'admin', '1', 'F10079D3840F4411A324B8DF7483FE3F');
 
 -- ----------------------------
 -- Table structure for athlete
@@ -95,7 +95,7 @@ CREATE TABLE `athleteteam` (
 -- ----------------------------
 -- Records of athleteteam
 -- ----------------------------
-INSERT INTO `athleteteam` VALUES (''4021A14ECBEF40C3B4150077A5FA9690'', ''gs'', ''内蒙古大学'', ''0'');
+INSERT INTO `athleteteam` VALUES ('4021A14ECBEF40C3B4150077A5FA9690', 'gs', '内蒙古大学', '0');
 
 -- ----------------------------
 -- Table structure for competition
@@ -118,8 +118,8 @@ CREATE TABLE `competition` (
 -- ----------------------------
 -- Records of competition
 -- ----------------------------
-INSERT INTO `competition` VALUES (''B1CEA16D65384C169030B16A00BC3AC6'', ''B47507AE6987430E98BBE646D17350A8'', ''足球'', ''1534C519F87E4A1B97584CAD86AEF0D8'', ''2019-01-15 11:41:52.000000'', ''2019-01-19 11:41:52.000000'');
-INSERT INTO `competition` VALUES (''C94FC7B4098B4336907B7C15003FCDAD'', ''B47507AE6987430E98BBE646D17350A8'', ''篮球'', ''1534C519F87E4A1B97584CAD86AEF0D8'', ''2019-01-07 13:17:11.000000'', ''2019-01-10 13:17:11.000000'');
+INSERT INTO `competition` VALUES ('B1CEA16D65384C169030B16A00BC3AC6', 'B47507AE6987430E98BBE646D17350A8', '足球', 'E339641B65A049CEB7105840836D9584', '2019-01-15 11:41:52.000000', '2019-01-19 11:41:52.000000');
+INSERT INTO `competition` VALUES ('C94FC7B4098B4336907B7C15003FCDAD', 'B47507AE6987430E98BBE646D17350A8', '篮球', '1534C519F87E4A1B97584CAD86AEF0D8', '2019-01-07 13:17:11.000000', '2019-01-10 13:17:11.000000');
 
 -- ----------------------------
 -- Table structure for competitionfield
@@ -136,9 +136,9 @@ CREATE TABLE `competitionfield` (
 -- ----------------------------
 -- Records of competitionfield
 -- ----------------------------
-INSERT INTO `competitionfield` VALUES (''00C3FDFAB9DE466781B982F314CEEBBD'', ''体育馆'', ''内蒙古123'', ''占有'');
-INSERT INTO `competitionfield` VALUES (''1534C519F87E4A1B97584CAD86AEF0D8'', ''内蒙古大学体育馆'', ''内蒙古大学'', ''空闲'');
-INSERT INTO `competitionfield` VALUES (''E339641B65A049CEB7105840836D9584'', ''文体馆'', ''内蒙古大学'', ''空闲'');
+INSERT INTO `competitionfield` VALUES ('00C3FDFAB9DE466781B982F314CEEBBD', '体育馆', '内蒙古自治区', '占有');
+INSERT INTO `competitionfield` VALUES ('1534C519F87E4A1B97584CAD86AEF0D8', '内蒙古大学体育馆', '内蒙古大学', '空闲');
+INSERT INTO `competitionfield` VALUES ('E339641B65A049CEB7105840836D9584', '文体馆', '内蒙古大学', '空闲');
 
 -- ----------------------------
 -- Table structure for competitionstage
@@ -153,8 +153,8 @@ CREATE TABLE `competitionstage` (
 -- ----------------------------
 -- Records of competitionstage
 -- ----------------------------
-INSERT INTO `competitionstage` VALUES (''B47507AE6987430E98BBE646D17350A8'', ''初赛'');
-INSERT INTO `competitionstage` VALUES (''BA28BA6C1D7D421796C39C2BF3F397F8'', ''决赛'');
+INSERT INTO `competitionstage` VALUES ('B47507AE6987430E98BBE646D17350A8', '初赛');
+INSERT INTO `competitionstage` VALUES ('BA28BA6C1D7D421796C39C2BF3F397F8', '决赛');
 
 -- ----------------------------
 -- Table structure for ranking
@@ -210,10 +210,10 @@ CREATE TABLE `role` (
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES (''4E495D120C40434EAF8B103DA6195E7F'', ''运动员'', ''4'');
-INSERT INTO `role` VALUES (''6A1DAA858C1E4EB1BB612D9939416CA4'', ''计分员'', ''2'');
-INSERT INTO `role` VALUES (''A5B522FF23A146DB94D1D16A6D13AD18'', ''裁判员'', ''3'');
-INSERT INTO `role` VALUES (''F10079D3840F4411A324B8DF7483FE3F'', ''管理员'', ''1'');
+INSERT INTO `role` VALUES ('4E495D120C40434EAF8B103DA6195E7F', '运动员', '4');
+INSERT INTO `role` VALUES ('6A1DAA858C1E4EB1BB612D9939416CA4', '计分员', '2');
+INSERT INTO `role` VALUES ('A5B522FF23A146DB94D1D16A6D13AD18', '裁判员', '3');
+INSERT INTO `role` VALUES ('F10079D3840F4411A324B8DF7483FE3F', '管理员', '1');
 
 -- ----------------------------
 -- Table structure for scoringstaff
